84c145a0f3a36b0380e2b4df10f431caa959a004
Luca Hofmann <HofmannLuca@gmail.com>
2015-12-16 19:20:41 +0100

    Refactorings

--- editor/src/com/nukethemoon/tools/opusproto/editor/app/Editor.java
+++ editor/src/com/nukethemoon/tools/opusproto/editor/app/Editor.java
@@ -5,14 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -55,7 +50,6 @@ import com.nukethemoon.tools.opusproto.editor.ui.sampler.formular.CombinedSample
 import com.nukethemoon.tools.opusproto.editor.ui.sampler.formular.FlatSamplerForm;
 import com.nukethemoon.tools.opusproto.editor.ui.sampler.formular.MaskedSamplerForm;
 import com.nukethemoon.tools.opusproto.editor.ui.sampler.formular.noise.NoiseSamplerForm;
-import com.nukethemoon.tools.opusproto.editor.ui.sampler.previews.SamplerPreviewImage;
 import com.nukethemoon.tools.opusproto.editor.ui.windows.SamplerEditor;
 import com.nukethemoon.tools.opusproto.exceptions.SamplerInvalidConfigException;
 import com.nukethemoon.tools.opusproto.exceptions.SamplerRecursionException;
@@ -101,60 +95,36 @@ import java.util.concurrent.Callable;
 
 public class Editor implements ApplicationListener, ChunkListener {
 
-	private SpriteBatch batch;
-	private ShapeRenderer worldShapeRenderer;
-	private ShapeRenderer screenShapeRenderer;
-	private OrthographicCamera camera;
-
-	private Map<Layer, List<Sprite>> layerToSprites = new HashMap<Layer, List<Sprite>>();
-
 	public static Map<Class<? extends AbstractSampler>, Class<? extends AbstractSamplerForm>> KNOWN_EDITORS;
 	public static Map<String, Class<? extends AbstractNoiseAlgorithm>> KNOWN_ALGORITHMS = new HashMap<String, Class<? extends AbstractNoiseAlgorithm>>();
-
-	private float blinkProgress = 0f;
-	private Color blinkColor = new Color();
 
 	private static LwjglApplicationConfiguration cfg;
 	private UI ui;
 
+	private Algorithms algorithms;
+	private Samplers samplers;
 	private Opus opus;
 
 	private int windowWidth;
 	private int windowHeight;
 
-	private boolean chunkRendererEnabled = false;
-
 	private static Bus bus = new Bus(ThreadEnforcer.ANY);
 
-	public static final String DEFAULT_SAMPLER_NAME = "EditorDefaultSampler";
-	public static final String DEFAULT_MASK_SAMPLER_NAME = "EditorDefaultMask";
-	public static final String DEFAULT_INTERPRETER_NAME = "EditorDefaultInterpreter";
-	public static final String DEFAULT_LAYER_NAME = "EditorDefaultLayer";
+	public static final String DEFAULT_SAMPLER_NAME = 		"EditorDefaultSampler";
+	public static final String DEFAULT_MASK_SAMPLER_NAME = 	"EditorDefaultMask";
+	public static final String DEFAULT_INTERPRETER_NAME = 	"EditorDefaultInterpreter";
+	public static final String DEFAULT_LAYER_NAME = 		"EditorDefaultLayer";
 
-	private Algorithms algorithms;
-
-	private Samplers samplers;
-
-	private Vector3 tmpVector0 = new Vector3();
-	private Vector3 tmpVector1 = new Vector3();
-	private Vector3 tmpVector2 = new Vector3();
-	private Vector3 tmpVector3 = new Vector3();
-	private Vector3 tmpVector4 = new Vector3();
-
-	private Sprite samplerSnapshot = null;
-	private boolean samplerSnapshotDrawBehind = true;
 	public static Stage STAGE;
+
 	private JsonLoader fileOperation;
 	private InputMultiplexer inputMultiplexer;
 	private InputController inputController;
 	private float performanceFactor = 1;
 
-	private Color clearColor = Color.GRAY;
-
-	private Ani ani;
-
 	private static Settings settings;
 	private Image splashScreen;
+	private OpusRenderer renderer;
 
 
 	public static void main(String [] args)	{
@@ -184,14 +154,9 @@ public class Editor implements ApplicationListener, ChunkListener {
 		windowWidth = cfg.width;
 		Gdx.graphics.setDisplayMode(windowWidth, windowHeight, false);
 
-		camera = new OrthographicCamera(windowWidth, windowHeight);
-		camera.position.set(0, 0, 10);
-		batch = new SpriteBatch();
 		STAGE = new Stage(new ScreenViewport());
-		worldShapeRenderer = new ShapeRenderer();
-		worldShapeRenderer.setAutoShapeType(true);
-		screenShapeRenderer = new ShapeRenderer();
-		screenShapeRenderer.setAutoShapeType(true);
+		renderer = new OpusRenderer(windowWidth, windowHeight, STAGE);
+
 		inputMultiplexer = new InputMultiplexer();
 		inputMultiplexer.addProcessor(STAGE);
 		Gdx.input.setInputProcessor(inputMultiplexer);
@@ -226,8 +191,6 @@ public class Editor implements ApplicationListener, ChunkListener {
 			}
 		};
 
-		ani = new Ani();
-
 		setExitDialog();
 
 		if (!Config.DEBUG) {
@@ -248,7 +211,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 		splashScreen.setPosition(cfg.width / 2 - splashScreen.getWidth() / 2,
 				cfg.height / 2 - splashScreen.getHeight() / 2);
 		STAGE.addActor(splashScreen);
-		ani.add(new BaseAnimation(1500, listener) {
+		renderer.getAni().add(new BaseAnimation(1500, listener) {
 			@Override
 			protected void onProgress(float v) {
 				Color imageColor = splashScreen.getColor();
@@ -357,10 +320,11 @@ public class Editor implements ApplicationListener, ChunkListener {
 		ui = new UI(STAGE, cfg, opus, algorithms, samplers, bus, Styles.UI_SKIN, settings);
 		bus.register(ui);
 		requestFirstChunks(opus);
-		chunkRendererEnabled = true;
-		inputController = new InputController(STAGE, camera, ui, opus, cfg);
+		renderer.setChunkRendering(true);
+		inputController = new InputController(STAGE, renderer.getCamera(), ui, opus, cfg);
 		inputMultiplexer.addProcessor(inputController);
-		clearColor = Color.BLACK;
+		renderer.setInputController(inputController);
+		renderer.setClearColor(Color.BLACK);
 	}
 
 	private void createEditorDefaultSampler(Samplers sam, Algorithms alg, double opusSeed) {
@@ -433,7 +397,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 			LayerConfig config = new LayerConfig(command.layerId);
 			config.interpreterId = DEFAULT_INTERPRETER_NAME;
 			Layer layer = new Layer(config, opus.getConfig().seed, samplers);
-			layerToSprites.put(layer, new ArrayList<Sprite>());
+			renderer.putLayerSprite(layer, new ArrayList<Sprite>());
 			opus.getLayers().add(layer);
 			Editor.post(new EventLayersChanged());
 
@@ -471,14 +435,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 	public void update(CommandGenerateWorld command) {
 		if (command.force || settings.autoRefresh) {
 			opus.clear();
-
-			for (Layer l : opus.getLayers()) {
-				for (Sprite s : layerToSprites.get(l)) {
-					s.getTexture().dispose();
-				}
-			}
-
-			layerToSprites.clear();
+			renderer.dispose();
 			requestFirstChunks(opus);
 		}
 	}
@@ -499,7 +456,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 	@Subscribe
 	@SuppressWarnings("unused")
 	public void clearSnapShot(CommandSnapshotDelete command) {
-		samplerSnapshot = null;
+		renderer.clearSnapshot();
 	}
 
 
@@ -521,33 +478,12 @@ public class Editor implements ApplicationListener, ChunkListener {
 	@Subscribe
 	@SuppressWarnings("unused")
 	public void endRect(CommandDrawMap command) {
-		tmpVector4.set(command.x1, command.y1, 0);
-		camera.unproject(tmpVector4);
-		int x1 = worldToMap(tmpVector4.x);
-		int y1 = worldToMap(tmpVector4.y);
-		tmpVector4.set(command.x2, command.y2, 0);
-		camera.unproject(tmpVector4);
-		int x2 = worldToMap(tmpVector4.x);
-		int y2 = worldToMap(tmpVector4.y);
-
-		int lowX = x1 < x2 ? x1 : x2;
-		int highX = x1 > x2 ? x1 : x2;
-		int lowY = y1 < y2 ? y1 : y2;
-		int highY = y1 > y2 ? y1 : y2;
-
-		commandDrawRectangle = null;
-		if ((highX - lowX) * (highY - lowY) > opus.getConfig().mapSize * (50 * performanceFactor)) {
-			return;
-		}
-
-		List<int[]> positions = new ArrayList<int[]>();
-		for (int x = lowX; x <= lowX + (highX - lowX); x++) {
-			for (int y = lowY; y <= lowY + (highY - lowY); y++) {
-				positions.add(new int[]{x, y});
-			}
-		}
+		List<int[]> positions = renderer.getChunkPositionsInRect(command.x1, command.y1,
+				command.x2, command.y2,
+				opus.getConfig().mapSize, performanceFactor);
 		requestChunks(positions);
 
+		commandDrawRectangle = null;
 	}
 
 	private void requestChunks(List<int[]> positions) {
@@ -563,43 +499,25 @@ public class Editor implements ApplicationListener, ChunkListener {
 
 	private void takeSnapshot(AbstractSampler sampler, boolean drawBehind, float opacity) {
 		if (opacity == 0) {
-			samplerSnapshot = null;
+			renderer.clearSnapshot();
 			return;
 		}
 		ui.getSnapshotTable().setVisible(true);
-		float zoom = camera.zoom;
-		if (camera.zoom > 2.5) {
-			zoom = 2.5f;
-		}
-		samplerSnapshotDrawBehind = drawBehind;
-		int w = (int) (windowWidth * (zoom));
-		int h = (int) (windowHeight * (zoom));
-		int offsetX = (int) ((-w / 2) + camera.position.x);
-		int offsetY = (int) ((-h / 2) + camera.position.y);
-		Pixmap pixmap = SamplerPreviewImage.create(sampler, w, h, 1, offsetX, offsetY);
-		samplerSnapshot = new Sprite(new Texture(pixmap));
-		samplerSnapshot.setAlpha(opacity);
-		samplerSnapshot.setPosition(offsetX, offsetY);
+		renderer.createSnapshot(drawBehind, windowWidth, windowHeight, sampler, opacity);
 	}
-
-
 
 	@Subscribe
 	@SuppressWarnings("unused")
 	public void changeSnapshotOrder(CommandSnapshotChangeSettings command) {
-		samplerSnapshotDrawBehind = command.drawBehind;
-		samplerSnapshot.setAlpha(command.opacity);
+		renderer.setSnapshotBehind(command.drawBehind);
+		renderer.setSnapshotAlpha(command.opacity);
 	}
 
 	private void requestFirstChunks(Opus op) {
 		int startSize = 4;
 
-		layerToSprites.clear();
-		for (Layer l : op.getLayers()) {
-			if (layerToSprites.get(l) == null) {
-				layerToSprites.put(l, new ArrayList<Sprite>());
-			}
-		}
+		renderer.dispose();
+		renderer.initLayerSprite(op.getLayers());
 
 		List<int[]> positions = new ArrayList<int[]>();
 		for (int mx = -startSize; mx < startSize; mx ++) {
@@ -653,7 +571,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 					result.dispose();
 					sprite.setPosition(chunk.getOffsetX(), chunk.getOffsetY());
 					Layer layer = opus.getLayers().get(finalLayerIndex);
-					layerToSprites.get(layer).add(sprite);
+					renderer.addSprite(layer, sprite);
 				}
 			});
 		}
@@ -698,9 +616,9 @@ public class Editor implements ApplicationListener, ChunkListener {
 		if (ui != null) {
 			ui.updatePosition(windowWidth, windowHeight);
 		}
-		camera.viewportWidth = windowWidth;
-		camera.viewportHeight = windowHeight;
-		camera.update();
+		renderer.getCamera().viewportWidth = windowWidth;
+		renderer.getCamera().viewportHeight = windowHeight;
+		renderer.getCamera().update();
 	}
 
 	@Subscribe
@@ -829,188 +747,22 @@ public class Editor implements ApplicationListener, ChunkListener {
 
 	@Override
 	public void render() {
+		int mapSize = -1;
+		List<Layer> layers = null;
 
-		handleCameraMovement();
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		worldShapeRenderer.setProjectionMatrix(camera.combined);
-		screenShapeRenderer.setProjectionMatrix(STAGE.getCamera().combined);
-
-		Gdx.gl.glClearColor(clearColor.r, clearColor.g, clearColor.b, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
-
-		if (chunkRendererEnabled) {
-			blinkProgress = (blinkProgress + 0.04f) % 1f;
-			drawGrid();
-			batch.begin();
-			if (samplerSnapshot != null && samplerSnapshotDrawBehind) {
-				samplerSnapshot.draw(batch);
-			}
-			for (Layer l : opus.getLayers()) {
-				for (Sprite sprite : layerToSprites.get(l)) {
-					sprite.draw(batch);
-				}
-			}
-
-			if (samplerSnapshot != null && !samplerSnapshotDrawBehind) {
-				samplerSnapshot.draw(batch);
-			}
-			batch.end();
-
-
-			drawOrigin();
-			drawTileSelection();
-			drawCameraCenter();
-			drawSelectionRect();
-
-			drawPoints();
-
-		}
-		ani.update();
-
-		STAGE.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-		STAGE.draw();
-	}
-
-	private void drawPoints() {
-
-		/*screenShapeRenderer.begin();
-
-		for (int index = 0; index < points.length; index = index + 2) {
-			float x = points[index];
-			float y = points[index + 1];
-			tmpVector1.set(x, y, 0);
-			camera.project(tmpVector1);
-
-			screenShapeRenderer.setColor(Color.BLUE);
-			screenShapeRenderer.rect(tmpVector1.x - 1, tmpVector1.y - 1, 1, 1);
-
-		}
-		screenShapeRenderer.end();*/
-	}
-
-	private void drawSelectionRect() {
-		if (commandDrawRectangle != null) {
-			screenShapeRenderer.begin();
-			float y1 = windowHeight - commandDrawRectangle.y1;
-			float x1 = commandDrawRectangle.x1;
-			float height = (windowHeight - commandDrawRectangle.y2) - y1;
-			float width = commandDrawRectangle.x2 - commandDrawRectangle.x1;
-			screenShapeRenderer.rect(x1, y1, width, height);
-			screenShapeRenderer.end();
-		}
-	}
-
-	private void drawOrigin() {
-		tmpVector2.set(0, 0, 0);
-		camera.project(tmpVector2, 0, 0, windowWidth, windowHeight);
-		screenShapeRenderer.begin();
-		screenShapeRenderer.setColor(Color.RED); // x
-		screenShapeRenderer.line(tmpVector2.x, tmpVector2.y, 0, tmpVector2.x + 80, tmpVector2.y, 0);
-		screenShapeRenderer.line(tmpVector2.x, tmpVector2.y + 1, 0, tmpVector2.x + 80, tmpVector2.y + 1, 0);
-		screenShapeRenderer.setColor(Color.YELLOW); // y
-		screenShapeRenderer.line(tmpVector2.x, tmpVector2.y, 0, tmpVector2.x, tmpVector2.y + 80, 0);
-		screenShapeRenderer.line(tmpVector2.x + 1, tmpVector2.y, 0, tmpVector2.x + 1, tmpVector2.y + 80, 0);
-		screenShapeRenderer.end();
-	}
-
-	private void drawTileSelection() {
-		int x = Math.round(inputController.getMouseWorldPosition().x - 0.5f);
-		int y = Math.round(inputController.getMouseWorldPosition().y - 0.5f);
-		tmpVector3.set(x, y, 0);
-		camera.project(tmpVector3);
-		screenShapeRenderer.begin();
-		blinkColor.set(blinkProgress, blinkProgress, blinkProgress, 1f);
-		screenShapeRenderer.setColor(blinkColor); // x
-		screenShapeRenderer.rect(tmpVector3.x, tmpVector3.y, 1 / camera.zoom, 1 / camera.zoom);
-		screenShapeRenderer.end();
-	}
-
-	private void drawGrid() {
-		screenShapeRenderer.begin();
-		tmpVector1.set(camera.position.x - ((windowWidth / 2) * camera.zoom),
-				camera.position.y + ((windowHeight / 2) * camera.zoom), 0);
-		camera.project(tmpVector1);
-		float screenLeft = tmpVector1.x;
-		float screenTop = tmpVector1.y;
-		tmpVector1.set(camera.position.x + ((windowWidth / 2) * camera.zoom),
-				camera.position.y - ((windowHeight / 2) * camera.zoom), 0);
-		camera.project(tmpVector1);
-		screenShapeRenderer.setColor(Styles.GRID_COLOR_DARK);
-		float screenRight = tmpVector1.x;
-		float screenBottom = tmpVector1.y;
-		float screenWidth = Math.abs(screenRight - screenLeft);
-		float screenHeight = Math.abs(screenTop + screenBottom);
-		int linesCountX = (int) ((screenWidth * camera.zoom) / opus.getConfig().mapSize) + 4;
-		int linesCountY = (int) ((screenHeight * camera.zoom) / opus.getConfig().mapSize) + 4;
-		int linesOffsetX = (int) (camera.position.x / opus.getConfig().mapSize);
-		int linesOffsetY = (int) (camera.position.y / opus.getConfig().mapSize);
-		if (linesCountX < 100 && linesCountY < 100) {
-			for (int x = -linesCountX / 2; x < linesCountX / 2; x++) {
-				for (int y = -linesCountY / 2; y < linesCountY / 2; y++) {
-					int offsetX = x + linesOffsetX;
-					int offsetY = y + linesOffsetY;
-					// horizontal lines
-					tmpVector0.set(camera.position.x - screenWidth * camera.zoom, opus.getConfig().mapSize * offsetY, 0);
-					tmpVector1.set(camera.position.x + screenWidth * camera.zoom, opus.getConfig().mapSize * offsetY, 0);
-					camera.project(tmpVector1);
-					camera.project(tmpVector0);
-					screenShapeRenderer.line(tmpVector0.x, tmpVector0.y, tmpVector1.x, tmpVector1.y);
-					// vertical lines
-					tmpVector0.set(opus.getConfig().mapSize * offsetX, camera.position.y - screenHeight * camera.zoom, 0);
-					tmpVector1.set(opus.getConfig().mapSize * offsetX, camera.position.y + screenHeight * camera.zoom, 0);
-					camera.project(tmpVector1);
-					camera.project(tmpVector0);
-					screenShapeRenderer.line(tmpVector0.x, tmpVector0.y, tmpVector1.x, tmpVector1.y);
-				}
-			}
-		}
-		screenShapeRenderer.end();
-	}
-
-	private void drawCameraCenter() {
-		screenShapeRenderer.begin();
-		blinkColor.set(1f - blinkProgress, 1f - blinkProgress, 1f - blinkProgress, 1f);
-		screenShapeRenderer.setColor(blinkColor); // x
-		float screenCenterX = windowWidth / 2;
-		float screenCenterY = windowHeight / 2;
-		screenShapeRenderer.line(screenCenterX - 10, screenCenterY, screenCenterX + 10, screenCenterY);
-		screenShapeRenderer.line(screenCenterX, screenCenterY - 10, screenCenterX, screenCenterY + 10);
-		screenShapeRenderer.end();
-
-	}
-
-	private void handleCameraMovement() {
-		if (inputController == null) {
-			return;
+		if (opus != null) {
+			mapSize = opus.getConfig().mapSize;
+			layers = opus.getLayers();
 		}
 
-		for (Integer keycode : inputController.getKeysDown()) {
-			float movement = camera.zoom * 4;
-			if (keycode == 19) { // up
-				camera.position.add(0, movement, 0);
-			}
-			if (keycode == 20) { // down
-				camera.position.add(0, -movement, 0);
-			}
-			if (keycode == 21) { // left
-				camera.position.add(-movement, 0, 0);
-			}
-			if (keycode == 22) { // right
-				camera.position.add(movement, 0, 0);
+		if (renderer != null) {
+			renderer.render(mapSize, layers);
+			if (commandDrawRectangle != null) {
+				renderer.drawSelectionRect(commandDrawRectangle.x1,
+						commandDrawRectangle.y1, commandDrawRectangle.x2,
+						commandDrawRectangle.y2);
 			}
 		}
-	}
-
-
-	private int worldToMap(float coordinate) {
-		int x = (int) (coordinate / opus.getConfig().mapSize);
-		if (coordinate < 0) {
-			x = x-1;
-		}
-		return x;
 	}
 
 
@@ -1035,6 +787,7 @@ public class Editor implements ApplicationListener, ChunkListener {
 		if (fileOperation != null) {
 			Settings.save(settings, Config.SETTINGS_FILE);
 		}
+		renderer.dispose();
 	}
 
 
@@ -1136,13 +889,4 @@ public class Editor implements ApplicationListener, ChunkListener {
 			}
 		}
 	}
-
-	private void updateWorldName(String name) {
-		opus.getConfig().name = name;
-		ui.getWorldEditor().setWorldName(name);
-		ui.getProjectWindow().setWorldName(name);
-	}
-
-
-
 }
